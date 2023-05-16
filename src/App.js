c2b3db25f394f8e6e6ddfca0c65e6df5365841a4
Tarun Suresh <tarunsuresh.v@gmail.com>
2023-05-16 22:16:12 +0530

    Fixed Bugs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
 import NotFound from './components/NotFound'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={LoginForm} />
       <Route component={NotFound} />
     </Switch>
-  </BrowserRouter>
 )
 
 export default App
